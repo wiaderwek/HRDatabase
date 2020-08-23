edbc2fe27a85bf053f018c39b15d229eab3137a6
Tomasz Wiaderek <t.wiaderwek@gmail.com>
2020-08-23 20:22:15 +0200

    Add generic class for other DAO classes

---
+++ src/main/java/database/GenericDAO.java
@@ -0,0 +1,35 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Class to be extended by other DAO classes
+ */
+public abstract class GenericDAO {
+
+    protected final String tableName;
+    protected Connection connection;
+
+    protected abstract Object mapToObject(ResultSet res) throws SQLException;
+
+    protected Collection mapToObjects(final ResultSet res) throws SQLException {
+        Collection result = new ArrayList();
+        while (res.next()) {
+            result.add(mapToObjects(res));
+        }
+
+        return result;
+    }
+
+    protected GenericDAO(Connection connection, String tableName) {
+        this.tableName = tableName;
+        this.connection = connection;
+    }
+
+
+}
